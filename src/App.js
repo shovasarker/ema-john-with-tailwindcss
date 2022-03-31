82e451aa2b17cae748bdcc922bb00c45ee880a1f
ShovaSarker <shovasarker01@gmail.com>
2022-03-31 23:08:44 +0600

    NotFound Component added for Unwanted paths and _redirects added for netlify deployment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom'
 import Header from './components/Header'
+import NotFound from './components/NotFound'
 import Orders from './pages/Orders'
 import Shop from './pages/Shop'
 
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path='/shop' element={<Shop />} />
         <Route path='/orders' element={<Orders />} />
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </div>
   )
---
+++ src/components/NotFound/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div className='h-96 w-screen flex justify-center items-center'>
+      <h2 className='text-center text-3xl font-bold font-mono'>
+        404 | Page Not Found
+      </h2>
+    </div>
+  )
+}
+
+export default NotFound
